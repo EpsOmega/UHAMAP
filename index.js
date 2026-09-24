9996e12182fb8eba21e4771d5be906c972abe01a
EpsOmega <adobewebs@gmail.com>
2026-09-24 13:25:28 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -14,6 +14,22 @@ $(document).ready(function () {
 
 $('#lib').text(lib);
 
+/////////////////////////////////////////////////////////**********************************************************************\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+//NEW LOCATION ASSIGNMENTS PLACE UP AT THE TOP OF THE CODE AND THEN EXIT IF FOUND
+//ML1 thru ML25 needs to point to ------>   cell-MORTENSEN_F1_BOUNDPER_RECT1
+if(lib === "MORTENSEN" && loc==="BOUNDPER" && callNumberInRange(cleanCcn, "ML1", "ML25") ){
+   console.log( "cleanCcn", cleanCcn );
+//alert('hi');
+    $('#row1').show();
+    $('#msvg1').show();
+    highlightSvg('#cell-MORTENSEN_F1_BOUNDPER_RECT1');
+    $('#loc').text("Lower Level (1st Floor) Bound");
+    matchFound = true;
+    return;
+    
+}//if (lib === "MORTENSEN") {
+/////////////////////////////////////////////////////////**********************************************************************\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
 
 
 (function () {
@@ -35,22 +51,6 @@ $('#lib').text(lib);
 
 $('#ccn').text(decodedCcn);
 
-
-/////////////////////////////////////////////////////////**********************************************************************\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-//NEW LOCATION ASSIGNMENTS PLACE UP AT THE TOP OF THE CODE AND THEN EXIT IF FOUND
-//ML1 thru ML25 needs to point to ------>   cell-MORTENSEN_F1_BOUNDPER_RECT1
-if(lib === "MORTENSEN" && loc==="BOUNDPER" && callNumberInRange(cleanCcn, "ML1", "ML25") ){
-   console.log( "cleanCcn", cleanCcn );
-//alert('hi');
-    $('#row1').show();
-    $('#msvg1').show();
-    highlightSvg('#cell-MORTENSEN_F1_BOUNDPER_RECT1');
-    $('#loc').text("Lower Level (1st Floor) Bound");
-    matchFound = true;
-    return;
-    
-}//if (lib === "MORTENSEN") {
-/////////////////////////////////////////////////////////**********************************************************************\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
 
 
