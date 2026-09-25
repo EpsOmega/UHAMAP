c2493aadfbdbd95203d0ad3deff394bdf021abed
EpsOmega <adobewebs@gmail.com>
2026-09-25 05:01:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -19,11 +19,12 @@ $('#lib').text(lib);
 //ML1 thru ML25 needs to point to ------>   cell-MORTENSEN_F1_BOUNDPER_RECT1
 if(lib === "MORTENSEN" && loc==="BOUNDPER" && callNumberInRange(cleanCcn, "ML1", "ML25") ){
    console.log( "cleanCcn", cleanCcn );
-//alert('hi');
     $('#row1').show();
     $('#msvg1').show();
     highlightSvg('#cell-MORTENSEN_F1_BOUNDPER_RECT1');
     $('#loc').text("Lower Level (1st Floor) Bound Periodicals");
+    //cleanCcn
+    $('#ccn').text(decodedCcn);
     matchFound = true;
     return;
     
